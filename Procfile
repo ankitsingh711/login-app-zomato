e72456e76c534a221f5e88ba1442f18d80c94991
ankit711 <itsankitsingh711@edureka.co>
2022-06-15 10:18:30 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,4 @@
+<process type>: <command>
+web: bundle exec rails server -p $PORT
+web: lein run -m demo.web $PORT
+web: java -jar target/myapp-1.0.0.jar
--- controller/authController.js
+++ controller/authController.js
@@ -5,7 +5,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const config = require('../config');
 const User = require('../model/userSchema');
+
+
 const port = process.env.PORT || 3300;
+const host = '0.0.0.0';
 
 router.use(bodyParser.urlencoded({extended:true}));
 router.use(bodyParser.json())
